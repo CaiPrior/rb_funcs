7364da63062db6c6bd2a836b51692a24a319963a
CaiPrior <62333278+CaiPrior@users.noreply.github.com>
2020-04-01 16:22:42 +0100

    Uses coef(model) instead of tiday(model)

--- cai_base.R
+++ cai_base.R
@@ -2,11 +2,15 @@
 
 library(broom)
 library(openxlsx)
-# Here we use 'tidy' instead of 'lm' as the output is easier to manipulate
-# change 'lm1' to be the name of your model
-trimmed = tidy(lm1)[,-c(3:5)]
-trimmed<-data.frame(trimmed)
-# create an empty list for loop outputted df to go into
+# Make a list of predictors
+predictors = as.list(names(train))
+# Naming the "rownames" column of the coefficients so that we can use the column properly
+model_coeffs <- coef(model)
+model_coeffs <- data.frame(model_coeffs)
+model_coeffs <- cbind(Row.Names = rownames(model_coeffs), model_coeffs)
+rownames(model_coeffs) <- NULL
+names(model_coeffs) <- c("term","estimate")
+# Create an empty list for loop output to go into
 output <- list()
 # 'selected_vars1' must be a list of your predictors
 # here we make 1 table per predictor by looping a filer(where) function
